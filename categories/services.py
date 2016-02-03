2f556a1a4abaf7f5d7179170be3a57f483158cdf
Nishant Mandapaty <nishantmandapaty@gmail.com>
2016-02-03 02:18:51 -0800

    Handles rate limit error on API requests.

--- categories/services.py
+++ categories/services.py
@@ -9,10 +9,13 @@ SEARCH_URL = 'https://www.eventbriteapi.com/v3/events/search/'
 Returns a list of category objects
 '''
 def get_categories():
-    r = requests.get(CATEGORIES_URL, params={'token': TOKEN}).json()
-    categories = [Category(cat) for cat in r['categories']]
+    r = requests.get(CATEGORIES_URL, params={'token': TOKEN})
+    if r.status_code == 429: #hit rate limit
+        return None, "Hit the Eventbrite API rate limit. Try again in a bit."
 
-    return categories
+    categories = [Category(cat) for cat in r.json()['categories']]
+
+    return (categories, '')
 
 '''
 Returns a list of event objects for the given page, as well as the total number
@@ -24,10 +27,12 @@ def get_events(categories, page=1):
         'categories': ','.join(categories),
         'page': page
     }
-    r = requests.get(SEARCH_URL, params=params).json()
+    r = requests.get(SEARCH_URL, params=params)
+    if r.status_code == 429: #hit rate limit
+        return None, None, "Hit the Eventbrite API rate limit. Try again in a bit."
 
-    page_count = r['pagination']['page_count']
+    page_count = r.json()['pagination']['page_count']
 
     #only keep relevant fields
     events = [Event(e) for e in r['events']]
-    return (events, page_count)
+    return (events, page_count, '')
--- categories/views.py
+++ categories/views.py
@@ -6,18 +6,25 @@ from . import services
 
 def index(request):
     if request.method == "GET":
-        cats = services.get_categories()
+        cats, error = services.get_categories()
+        if error:
+            return HttpResponse(error)
+
         return render(request, 'search.html',
                       context={'categories': cats})
 
 def events(request):
-    choices = [
-        request.GET.get('c1', ''),
-        request.GET.get('c2', ''),
-        request.GET.get('c3', '')
-    ]
-    page = int(request.GET.get('page', '1'))
+    if request.method == "GET":
+        choices = [
+            request.GET.get('c1', ''),
+            request.GET.get('c2', ''),
+            request.GET.get('c3', '')
+        ]
+        page = int(request.GET.get('page', '1'))
 
-    events, page_count = services.get_events(choices, page=page)
-    return render(request, 'events.html',
-                  context={'events': events, 'page_count': page_count, 'current_page': page})
+        events, page_count, error = services.get_events(choices, page=page)
+        if error:
+            return HttpResponse(error)
+
+        return render(request, 'events.html',
+                      context={'events': events, 'page_count': page_count, 'current_page': page})
